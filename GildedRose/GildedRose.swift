981f3c89184b292b210658910f97fbb2ae8b43f8
Jose Servet Font <jose.servet.font@everis.com>
2019-11-11 08:56:08 +0100

    Refactoring después de la sesión

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -1,5 +1,8 @@
 
 public class GildedRose {
+    let agedBrie = "Aged Brie"
+    let backstagePass = "Backstage passes to a TAFKAL80ETC concert"
+    let sulfuras = "Sulfuras, Hand of Ragnaros"
     var items:[Item]
     
     required public init(items:[Item]) {
@@ -9,50 +12,47 @@ public class GildedRose {
     public func updateQuality() {
         
         for i in 0..<items.count {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
+            if (items[i].name == agedBrie) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
-                    
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
                 }
-            }
-            
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                 items[i].sellIn = items[i].sellIn - 1
-            }
-            
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
+                if (items[i].sellIn < 0){
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
+                    }
+                }
+
+
+            } else if (items[i].name == backstagePass) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
+
+                    if (items[i].sellIn < 11) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
+                        }
+                    }
+
+                    if (items[i].sellIn < 6) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
+                        }
+                    }
+
+                }
+                items[i].sellIn = items[i].sellIn - 1
+                if (items[i].sellIn < 0){
+                    items[i].quality = 0
+                }
+            } else {
+                if (items[i].quality > 0) {
+                    if (items[i].name != sulfuras) {
+                        items[i].quality = items[i].quality - 1
+                        items[i].sellIn = items[i].sellIn - 1
+                        if (items[i].sellIn < 0){
+                            items[i].quality = items[i].quality - 1
+                        }
                     }
                 }
             }
--- GildedRoseTests/GildedRoseTests.swift
+++ GildedRoseTests/GildedRoseTests.swift
@@ -3,16 +3,117 @@ import XCTest
 
 class GildedRoseTests: XCTestCase {
 
-    func testFoo() {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
+    func testQualityOfRegularObject() {
+        let items = [Item(name: "foo", sellIn: 10, quality: 10)]
         let app = GildedRose(items: items);
         app.updateQuality();
-        XCTAssertEqual("fixme", app.items[0].name);
+        let item = app.items[0]
+        XCTAssertEqual(item.sellIn, 9);
+        XCTAssertEqual(item.quality, 9);
     }
 
-    static var allTests : [(String, (GildedRoseTests) -> () throws -> Void)] {
-        return [
-            ("testFoo", testFoo),
-        ]
+    func testQualityOfRegularObjectAfterSellInExpired() {
+        let items = [Item(name: "foo", sellIn: 0, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, 8);
     }
+
+    func testQualityOfRegularObjectCantHaveNegativeValue() {
+        let items = [Item(name: "foo", sellIn: 10, quality: 0)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, 0);
+    }
+
+    func testQualityOfRegularObjectAcceptInitialNegativeValues() {
+        let items = [Item(name: "foo", sellIn: 10, quality: -1)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, -1);
+    }
+
+    func testQualityOfAgedBrieIncreasesWithTime(){
+        let items = [Item(name: "Aged Brie", sellIn: 10, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0];
+        XCTAssertEqual(item.sellIn, 9)
+        XCTAssertEqual(item.quality, 11)
+    }
+
+    func testQualityOfAgedBrieIncreasesDoubleWithTimeWhenSellInExpired(){
+        let items = [Item(name: "Aged Brie", sellIn: 0, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0];
+        XCTAssertEqual(item.quality, 12)
+    }
+
+    func testQualityOfAgedBrieOverFifty() {
+        let items = [Item(name: "Aged Brie", sellIn: 10, quality: 50)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, 50);
+    }
+
+    func testInitialQualityOfAgedBrieOverFifty() {
+        let items = [Item(name: "Aged Brie", sellIn: 10, quality: 51)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, 51);
+    }
+
+    func testQualitySulfurasRemainsUnchanged(){
+        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 10, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0];
+        XCTAssertEqual(item.sellIn, 10)
+        XCTAssertEqual(item.quality, 10)
+    }
+
+    func testQualityOfBackstagePassesMoreThanTenDaysAway() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.sellIn, 10);
+        XCTAssertEqual(item.quality, 11);
+    }
+
+    func testQualityOfBackstagePassesMoreThanFiveDaysAway() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 8, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, 12);
+    }
+
+    func testQualityOfBackstagePassesLessThanFiveDaysAway() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 4, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, 13);
+    }
+
+    func testQualityOfBackstagePassesSellInExpired() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 10)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        let item = app.items[0]
+        XCTAssertEqual(item.quality, 0);
+    }
+
+//    static var allTests : [(String, (GildedRoseTests) -> () throws -> Void)] {
+//        return [
+//            ("testFoo", testFoo),
+//        ]
+//    }
 }
